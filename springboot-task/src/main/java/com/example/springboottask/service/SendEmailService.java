fd7af4081b1db6f4a0d54c95f163630e786ede25
13949 <1394949540@qq.com>
2021-01-19 12:19:46 +0800

    邮件任务

--- springboot-task/src/main/java/com/example/springboottask/service/SendEmailService.java
+++ springboot-task/src/main/java/com/example/springboottask/service/SendEmailService.java
@@ -3,14 +3,11 @@ package com.example.springboottask.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.mail.javamail.MimeMailMessage;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.io.File;
-import java.time.LocalDateTime;
 
 
 @Service
